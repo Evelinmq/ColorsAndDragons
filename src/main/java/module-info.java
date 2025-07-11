777ba76984726d6e60ae83780351fa00ab8951f2
Getse <20243ds149@utez.edu.mx>
2025-07-10 19:06:22 -0600

    conexion a la base de datos, dao edificio

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.integradora {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires ucp;
 
 
     opens com.example.integradora to javafx.fxml;
