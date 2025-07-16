f2a2701884492df8e71372bf1c24c4a3652ba9f3
Artur <20243ds154utez.edu.mx>
2025-07-15 20:22:58 -0600

    controllers de usuario y empleado terminados

--- src/main/java/com/example/integradora/controllers/UpdateEmpleadoController.java
+++ src/main/java/com/example/integradora/controllers/UpdateEmpleadoController.java
@@ -1,4 +1,57 @@
 package com.example.integradora.controllers;
 
-public class UpdateEmpleadoController {
+import com.example.integradora.modelo.Empleado;
+import com.example.integradora.modelo.dao.EmpleadoDao;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UpdateEmpleadoController implements Initializable {
+
+    @FXML
+    private TextField nombre;
+
+    @FXML
+    private TextField rfc;
+
+    private Empleado empleado;
+    private String rfcViejo;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+        this.rfcViejo = empleado.getRfc();
+
+        nombre.setText(empleado.getNombre());
+        rfc.setText(empleado.getRfc());
+    }
+
+    @FXML
+    public void updateEmpleado(ActionEvent event) {
+
+        String nuevoNombre = nombre.getText();
+        String nuevoRfc = rfc.getText();
+
+
+        empleado.setNombre(nuevoNombre);
+        empleado.setRfc(nuevoRfc);
+
+
+        EmpleadoDao dao = new EmpleadoDao();
+        if (dao.updateEmpleado(rfcViejo, empleado)) {
+            System.out.println("Empleado actualizado correctamente.");
+        }
+
+
+        Stage ventana = (Stage) nombre.getScene().getWindow();
+        ventana.close();
+    }
 }
--- src/main/java/com/example/integradora/controllers/UpdateUsuarioController.java
+++ src/main/java/com/example/integradora/controllers/UpdateUsuarioController.java
@@ -1,4 +1,88 @@
 package com.example.integradora.controllers;
 
-public class UpdateUsuarioController {
+import com.example.integradora.modelo.Usuario;
+import com.example.integradora.modelo.dao.UsuarioDao;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UpdateUsuarioController implements Initializable {
+
+
+    @FXML
+    private TextField correo;
+    @FXML
+    private TextField contrasena;
+    @FXML
+    private TextField rfcEmpleado;
+    @FXML
+    private TextField idUnidad;
+    @FXML
+    private TextField idPuesto;
+    @FXML
+    private TextField estado;
+
+
+    private Usuario usuario;
+    private String correoViejo;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+        this.correoViejo = usuario.getCorreo();
+
+
+        correo.setText(usuario.getCorreo());
+        contrasena.setText(usuario.getContrasena());
+        rfcEmpleado.setText(usuario.getRfcEmpleado());
+        idUnidad.setText(String.valueOf(usuario.getIdUnidad()));
+        idPuesto.setText(String.valueOf(usuario.getIdPuesto()));
+        estado.setText(String.valueOf(usuario.getEstado()));
+    }
+
+
+    @FXML
+    public void updateUsuario(ActionEvent event) {
+
+
+        String nuevoCorreo = correo.getText();
+        String nuevaContrasena = contrasena.getText();
+        String nuevoRfc = rfcEmpleado.getText();
+        int nuevaUnidad = Integer.parseInt(idUnidad.getText());
+        int nuevoPuesto = Integer.parseInt(idPuesto.getText());
+        int nuevoEstado = Integer.parseInt(estado.getText());
+
+
+        usuario.setCorreo(nuevoCorreo);
+        usuario.setContrasena(nuevaContrasena);
+        usuario.setRfcEmpleado(nuevoRfc);
+        usuario.setIdUnidad(nuevaUnidad);
+        usuario.setIdPuesto(nuevoPuesto);
+        usuario.setEstado(nuevoEstado);
+
+
+        UsuarioDao dao = new UsuarioDao();
+
+
+        if (dao.updateUsuario(correoViejo, usuario)) {
+            System.out.println("Usuario actualizado");
+        }
+
+
+        Stage ventana = (Stage) correo.getScene().getWindow();
+        ventana.close();
+    }
 }
+
+
