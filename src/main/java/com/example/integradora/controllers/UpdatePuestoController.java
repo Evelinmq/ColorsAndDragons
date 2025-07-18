641a0746af09188a93c8b14351599b9d18fa7328
Eve <Eve@yo.com>
2025-07-17 23:32:18 -0600

    Se corriegieron los controladores de editar Puesto y Unidad Administrativa

--- src/main/java/com/example/integradora/controllers/UpdatePuestoController.java
+++ src/main/java/com/example/integradora/controllers/UpdatePuestoController.java
@@ -2,10 +2,14 @@ package com.example.integradora.controllers;
 
 import com.example.integradora.modelo.Puesto;
 import com.example.integradora.modelo.dao.PuestoDao;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -17,28 +21,52 @@ public class UpdatePuestoController implements Initializable {
     @FXML
     private TextField puesto;
     @FXML
-    private Button actualizar;
+    private MenuButton editarPuesto;
 
     private Puesto p;
     private int idViejito;
+    private String nuevoNombrePuesto;
+
+    private ObservableList<String> opcionesPuesto = FXCollections.observableArrayList(
+            "Opción 1", "Opción 2", "Opción 3"
+    );
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        actualizar.setOnAction(( event) -> {
-            PuestoDao dao = new PuestoDao();
-            dao.updatePuesto(idViejito, new Puesto(null, puesto.getText()));
-
-            Stage ventana = (Stage) id.getScene().getWindow();
-            ventana.close();
-        });
+        for (String opcion : opcionesPuesto) {
+            MenuItem item = new MenuItem(opcion);
+            item.setOnAction(e -> {
+                nuevoNombrePuesto = opcion;
+                editarPuesto.setText(opcion);
+            });
+            editarPuesto.getItems().add(item);
+        }
     }
 
-    public void setP(Puesto p) {
+    public void setEspacio(Puesto p) {
         this.p = p;
-        //this.idViejito = p.getId();
+        this.idViejito = p.getId();
 
-        puesto.setText(p.getNombre());
-
+        editarPuesto.setText(nuevoNombrePuesto);
+        editarPuesto.setText(p.getNombre());
     }
 
+    @FXML
+    public void updatePuesto(ActionEvent event) {
+        String ediPuesto = editarPuesto.getText();
+
+        p.setId(idViejito);
+        p.setNombre(ediPuesto);
+
+        PuestoDao dao = new PuestoDao();
+
+        if (dao.updatePuesto(idViejito, p)) {
+            System.out.println("Puesto editado correctamente");
+        } else {
+            System.out.println("Error al actualizar el Puesto");
+        }
+
+        Stage ventana = (Stage) editarPuesto.getScene().getWindow();
+        ventana.close();
+    }
 }
--- src/main/java/com/example/integradora/controllers/UpdateUnidadController.java
+++ src/main/java/com/example/integradora/controllers/UpdateUnidadController.java
@@ -2,10 +2,14 @@ package com.example.integradora.controllers;
 
 import com.example.integradora.modelo.UnidadAdministrativa;
 import com.example.integradora.modelo.dao.UnidadAdministrativaDao;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -17,27 +21,53 @@ public class UpdateUnidadController implements Initializable {
     @FXML
     private TextField unidad;
     @FXML
-    private Button actualizar;
+    private MenuButton editarUnidad;
 
     private UnidadAdministrativa u;
     private int idViejito;
+    private String nuevoNombreUnidad;
+
+    private ObservableList<String> opcionesUnidad = FXCollections.observableArrayList(
+            "Opción 1", "Opción 2", "Opción 3"
+    );
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        actualizar.setOnAction((event) -> {
-            UnidadAdministrativaDao dao = new UnidadAdministrativaDao();
-            dao.updateUnidadAdministrativa(null, unidad.getText());
-
-            Stage ventana  = (Stage) id.getScene().getWindow();
-            ventana.close();
-        });
+        for (String opcion : opcionesUnidad) {
+            MenuItem item = new MenuItem(opcion);
+            item.setOnAction(e -> {
+                nuevoNombreUnidad = opcion;
+                editarUnidad.setText(opcion);
+            });
+            editarUnidad.getItems().add(item);
+        }
     }
 
-    public void setUnidad(UnidadAdministrativa u) {
+    public void setEspacio(UnidadAdministrativa u) {
         this.u = u;
         this.idViejito = u.getId();
 
-        unidad.setText(u.getNombre());
+        editarUnidad.setText(nuevoNombreUnidad);
+        editarUnidad.setText(u.getNombre());
+    }
+
+    @FXML
+    public void updateUnidad(ActionEvent event) {
+        String ediUnidad = editarUnidad.getText();
+
+        u.setId(idViejito);
+        u.setNombre(ediUnidad);
+
+        UnidadAdministrativaDao dao = new UnidadAdministrativaDao();
+
+        if (dao.updateUnidadAdministrativa(idViejito, u)) {
+            System.out.println("Unidad Administrativa editada correctamente");
+        } else {
+            System.out.println("Error al actualizar la Unidad Administrativa");
+        }
+
+        Stage ventana = (Stage) editarUnidad.getScene().getWindow();
+        ventana.close();
     }
 
 }
