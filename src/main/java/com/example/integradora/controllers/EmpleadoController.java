ba145257b03906e3cc6907856b6b97895d31b996
Artur <20243ds154utez.edu.mx>
2025-07-30 12:33:26 -0600

    corregi

--- src/main/java/com/example/integradora/controllers/EmpleadoController.java
+++ src/main/java/com/example/integradora/controllers/EmpleadoController.java
@@ -120,7 +120,7 @@ public class EmpleadoController implements Initializable {
 
     private void abrirVentanaRegistro() {
         try {
-            FXMLLoader loader = new FXMLLoader(EmpleadoController.class.getResource("/com/example/integradora/EditarEmpleados.fxml"));
+            FXMLLoader loader = new FXMLLoader(EmpleadoController.class.getResource("/com/example/integradora/RegistrarEmpleados.fxml"));
             Parent root = loader.load();
 
             Scene escenaPrincipal = agregar.getScene();
