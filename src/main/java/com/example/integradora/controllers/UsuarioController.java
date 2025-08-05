a95e1aa1974d8402931da942c6049e5200eb70ac
Artur <20243ds154utez.edu.mx>
2025-08-04 23:49:58 -0600

    corregi

--- src/main/java/com/example/integradora/controllers/UsuarioController.java
+++ src/main/java/com/example/integradora/controllers/UsuarioController.java
@@ -63,7 +63,7 @@ public class UsuarioController implements Initializable {
 
         tablaUsuarioCorreo.setCellValueFactory(new PropertyValueFactory<>("correo"));
         tablaUsuarioRol.setCellValueFactory(new PropertyValueFactory<>("rolDescripcion"));
-        tablaUsuarioContrasena.setCellValueFactory(new PropertyValueFactory<>("CONTRASENIA"));
+        tablaUsuarioContrasena.setCellValueFactory(new PropertyValueFactory<>("contrasena"));
         recargarTabla();
 
         ObservableList<Usuario> listaObservable = FXCollections.observableList(lista);
